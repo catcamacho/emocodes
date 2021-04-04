f407caf3c59077de852d2ae01146e8082c0f0515
Maria Catalina Camacho <m.catalina.camacho@gmail.com>
2021-04-04 15:09:25 -0500

    Update testing.py

--- testing/testing.py
+++ testing/testing.py
@@ -1,12 +1,1 @@
-from emocodes import GraphTimeSeries
-from pandas import read_csv
-from glob import glob
-from os.path import basename
-
-files = ['']
-
-for file in files:
-    filename = basename(file)[:-20]
-    file_df = read_csv(file, index_col=0)
-    gt = GraphTimeSeries(code_series_df=file_df)
-    gt.graph_all(num_outfile=filename+'_numeric', cat_outfile=filename+'_categorical', file_type='svg')
+# TODO: write testing module
