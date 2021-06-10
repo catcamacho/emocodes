f1e208aeac600dcbe5556b852cfb3f9f850c13f6
Maria Catalina Camacho <m.catalina.camacho@gmail.com>
2021-06-10 10:49:43 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ release = 'dev0.1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser', 'sphinx_rtd_theme', 'sphinx_toolbox.confval', 'sphinx.ext.autodoc',
+extensions = ['sphinx_rtd_theme', 'sphinx_toolbox.confval', 'sphinx.ext.autodoc',
               'sphinx.ext.coverage', 'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
