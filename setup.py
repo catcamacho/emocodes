84b8b2da704bc8a3e14f7dbae4e4ebf61f40d4a6
Cat Camacho <m.catalina.camacho@gmail.com>
2021-10-26 11:43:50 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='emocodes',
     packages=find_packages(),
-    version='0.1.2',
+    version='1.0.0',
     description='A library designed to accompany the EmoCodes system.',
     author='M. Catalina Camacho',
     license='MIT',
