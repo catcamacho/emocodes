93850705aaf6fa3e7fdf80d92483eed56669c1b0
Cat Camacho <m.catalina.camacho@gmail.com>
2021-10-26 11:47:18 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description='A library designed to accompany the EmoCodes system.',
     author='M. Catalina Camacho',
     license='MIT',
-    download_url='https://github.com/catcamacho/emocodes/',
+    download_url='https://github.com/catcamacho/emocodes/archive/refs/tags/v.1.0.0.tar.gz',
     install_requires=['pandas', 'numpy', 'moviepy', 'opencv-python', 'matplotlib', 'openpyxl', 'pliers',
                       'librosa', 'python-magic-bin==0.4.14', 'seaborn', 'pingouin', 'scipy', 'scikit-learn',
                       'markdown', 'weasyprint'],
